7343adb5972f08344995d63b11f227b118f21d05
Simon Sandvik Lee <simonsandviklee@gmail.com>
2023-09-16 16:56:48 +0200

    Add Router between pages

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+        <>
+            <h1>Welcome to the Homepage!</h1>
+        </>
+    );
+};
+
+export default HomePage;
---
+++ src/pages/WeatherPage.tsx
@@ -0,0 +1,9 @@
+const WeatherPage = () => {
+    return (
+        <>
+            <h1>Welcome to the Weather page!</h1>
+        </>
+    );
+};
+
+export default WeatherPage;
