748b96fcd0e4a429a3b72a4c8dc1b8b51c730218
Eduard Prokhorikhin <eduardpr@stud.ntnu.no>
2023-09-20 15:40:56 +0200

    Optimized weather page for smaller screens

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/project1',
   test: {
     globals: true,
     environment: 'jsdom',
